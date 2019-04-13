17764dd2d1c44ea1b44968c76c66bf66c219b255
Mochamad Taufan Rezzafri <rezzafri@gmail.com>
2019-04-13 23:20:53 +0700

    Change config.

--- config/index.js
+++ config/index.js
@@ -1,26 +1,26 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteTitleManifest: 'MinimalBlog',
-  siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
-  siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
+  siteTitle: 'IAI Kabupaten Blitar', // Navigation and Site Title
+  siteTitleAlt: 'Website Resmi - Ikatan Apoteker Kabupaten Blitar', // Alternative Site title for SEO
+  siteTitleManifest: 'IAIKabupatenBlitar',
+  siteUrl: 'https://iaikabupatenblitar.or.id', // Domain of your site. No trailing slash!
+  siteLanguage: 'id', // Language Tag on <html> element
+  siteHeadline: 'Ikatan Apoteker Kabupaten Blitar', // Headline for schema.org JSONLD
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  siteDescription: 'Website resmi IAI / Ikatan Apoteker Kabupaten Blitar Pimpinan Cabang Kabupaten Blitar.', // Your site description
+  author: 'IAIKabupatenBlitar', // Author for schemaORGJSONLD
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@minimal', // Twitter Username - Optional
   ogSiteName: 'minimal', // Facebook Site Name - Optional
-  ogLanguage: 'en_US', // Facebook Language
+  ogLanguage: 'id_ID', // Facebook Language
   googleAnalyticsID: 'UA-47519312-6',
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
-  themeColor: '#3498DB',
-  backgroundColor: '#2b2e3c',
+  themeColor: '#d02e77',
+  backgroundColor: 'white',
 }
